4c5f23828793b8ddf36175a7865a3c5e94b60dc4
wensong <wensong@673.com>
2024-01-22 21:48:59 +0800

    second commit

--- git_test/src/main/java/com/wensong/git_test/GitTest.java
+++ git_test/src/main/java/com/wensong/git_test/GitTest.java
@@ -9,5 +9,6 @@ package com.wensong.git_test;/*
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git1!");
     }
 }
