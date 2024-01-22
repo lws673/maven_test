ad407e46ef1604fd4b86632a597c422f6efbde22
wensong <wensong@673.com>
2024-01-22 21:41:36 +0800

    first commit

---
+++ git_test/src/main/java/com/wensong/git_test/GitTest.java
@@ -0,0 +1,13 @@
+package com.wensong.git_test;/*
+ *@title GitTest
+ *@description
+ *@author WenSong Liu
+ *@version 1.0
+ *@create 2024/1/22 21:37
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
